e7bff764e0b14ef32c30baf5afef0edb4ba03bcb
huilinhu <2449315521@qq.com>
2022-03-31 20:37:34 +0800

    first commit

---
+++ luhn.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class luhn {
+
+    public static void main(String[] args) {
+        String str="7992739871";
+        System.out.println(str);
+
+        if (check(str)){
+            System.out.println("校验成功");
+        }else {
+            System.out.println("校验失败");
+        }
+    }
+
+    private static boolean check(String digits) {
+        return check(numStringToArray(digits));
+    }
+
+
+    private static Integer[] numStringToArray(String numStr){
+        byte[] bytes=numStr.getBytes();
+        List<Integer> list=new ArrayList<>();
+        for (byte b:bytes){
+            list.add((int) b-48);
+        }
+        Integer[] digits=new Integer[list.size()];
+        list.toArray(digits);
+        return digits;
+    }
+
+    private static boolean check(Integer[] digits) {
+        int sum = 0;
+        int length = digits.length;
+        for (int i = 0; i < length; i++) {
+
+            int digit = digits[length - i - 1];
+
+            if (i % 2 == 1) {
+                digit *= 2;
+            }
+            sum += digit > 9 ? digit - 9 : digit;
+        }
+        return sum % 10 == 0;
+    }
+
+}
